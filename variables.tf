a5c64c639247bf3af097d073c2dc50dd4ec488e1
case0sh <cubikline@gmail.com>
2023-04-04 18:35:57 +0200

    udpate cicd

--- variables.tf
+++ variables.tf
@@ -6,30 +6,37 @@ variable "do_token" {
 variable "ssh_name" {
   description   = "Ssh keys in DigitalOcean"
     type        = string
+    default     = "case" 
 }
 variable "droplet_image" {
   description   = "Image identifier of the OS in DigitalOcean"
     type        = string
+    default     = "ubuntu-20-04-x64"
 }
 variable "droplet_region" {
   description = "Droplet region identifier where the droplet will be created"
     type        = string
+    default = "fra1"
 }
 variable "droplet_size" {
   description = "Droplet size identifier"
     type        = string
+    default = "s-1vcpu-2gb"
 }
 variable "user" {
   description = "Username"
     type        = string
+    default = "root"
 }
 variable "privatekeypath" {
   description = "Privat ssh key"
     type        = string
+    default = "~/.ssh/id_ed25519"
 }
 
 variable "publicekeypath" {
   description = "Public ssh key"
     type        = string
+    default = "~/.ssh/id_ed25519.pub"
 }
 
