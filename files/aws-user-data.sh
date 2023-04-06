5ebdc12832025cfe747502b3fd233bbce088dfd7
case0sh <cubikline@gmail.com>
2023-04-06 22:32:05 +0200

    udpate cicd

--- files/aws-user-data.sh
+++ files/aws-user-data.sh
@@ -8,7 +8,7 @@ sudo sh get-docker.sh
 sudo service docker start
 sudo apt install git httpie vim jq zip unzip -y
 
-
+cd /home/ubuntu/
 git clone https://github.com/Renegade-Master/zomboid-dedicated-server.git
 cd zomboid-dedicated-server
 docker compose -f docker-compose.yml up -d
