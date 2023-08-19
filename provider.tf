bf02c6e5bea52b98e5cb00d1a15bf00395566b41
case0sh <cubikline@gmail.com>
2023-08-19 14:37:20 +0200

    update

--- provider.tf
+++ provider.tf
@@ -8,6 +8,7 @@ terraform {
   # using GitLab http backend
   # see: https://docs.gitlab.com/ee/user/infrastructure/terraform_state.html
  backend "http" {
+  
  }
 ##    # auto-configured by the template
   required_providers {
